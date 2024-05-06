4273b9ae939f73b6a9fb3e520bd656d82060c8bc
am224ut <94832146+Albion007@users.noreply.github.com>
2024-05-06 23:06:56 +0100

    Admin folder, AddProduct.jsx file changed

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -14,7 +14,7 @@ const AddProduct = () => {
   return (
     <div className='add-product'>
       <div className="addproduct-itemfield">
-        <p>Product title</p>
+        <p>Productt title</p>
         <input type='text' name='name' placeholder='Type here' />
       </div>
       <div className="addproduct-price">
