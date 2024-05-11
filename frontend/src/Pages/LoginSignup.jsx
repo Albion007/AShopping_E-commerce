1bff04557f2ed57d0b1624d5c11c4fe34d7eca07
am224ut <94832146+Albion007@users.noreply.github.com>
2024-05-11 12:14:43 +0100

    login and signup finished

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
+
+  const [state,setState] = useState("Login");
+
+  
+
   return (
     <div className='loginsignup'>
       <div className='loginsignup-container'>
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type='text' placeholder='Name' />
+          {state==="Sign Up"?<input type='text' placeholder='Name' />:<></>}
           <input type='email' placeholder='Email' />
           <input type='password' placeholder='Passowrd' />
         </div>
         <button>Continue</button>
-        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        {state==="Sign Up"
+        ?<p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>
+        :<p className='loginsignup-login'>Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
         <div className="loginsignup-agree">
           <input type='checkbox' name='' id='' />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
