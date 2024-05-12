e14e68604d67c9b63a796d78b8bd069f5842e57c
am224ut <94832146+Albion007@users.noreply.github.com>
2024-05-12 22:30:06 +0100

    working on login and signup still

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -21,7 +21,20 @@ const LoginSignup = () => {
 
   const signup = async () =>{
     console.log("Signup function executed",formData);
+    let responseData;
+    await fetch('http://localhost:3000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=> response.json()).then((data)=>responseData=data)
   
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
   }
 
   return (
