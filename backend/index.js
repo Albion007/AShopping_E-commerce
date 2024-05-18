4747b73f3d3a50efa6c48027605713420e68f298
am224ut <94832146+Albion007@users.noreply.github.com>
2024-05-18 14:46:22 +0100

    ...

--- backend/index.js
+++ backend/index.js
@@ -230,11 +230,26 @@ const fetchUser = async (req,res,next)=>{
 
 // creating endpoint for adding products in cartdata
 app.post('/addtocart',fetchUser,async (req,res)=>{
-    
+    console.log("Added",req.body.itemId);
     let userData = await Users.findOne({_id:req.user.id});
     userData.cartData[req.body.itemId] += 1;
     await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
     res.send("Added")
+})
+
+//creating endpoint to remove product from cartdata
+app.post('/removefromcart',fetchUser,async (req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed")
+})
+
+//creating endpoint to get cartdata
+app.post('/getcart',fetchUser,async ()=>{
+    
 })
 
 app.listen(port,(error)=>{
